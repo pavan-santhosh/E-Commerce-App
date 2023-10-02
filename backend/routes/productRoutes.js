aea95f7b7460c3bdf33130a543abff05ca235576
pavan-santhosh <125680930+pavan-santhosh@users.noreply.github.com>
2023-10-02 16:16:53 +0530

    Creating Controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,24 +2,11 @@ import express from "express";
 import asyncHandler from '../middleware/asyncHandler.js';
 const router = express.Router();
 import Product from '../models/productModel.js'
+import { getProducts, getProductById } from "../controllers/productController.js";
 
 //@desc Fetch all products
-router.get('/', asyncHandler(async(req,res)=> {
-    const products = await Product.find({});// we want all the products, so no argument was passed
-    res.json(products);
-}));
+router.get('/', getProducts);
 
-router.get('/:id',asyncHandler(async(req,res)=>{
-    const product = await Product.findById(req.params.id);
-    if(product){
-        return res.json(product);
-    } 
-    else{
-        res.status(404);
-        throw new Error({message:'Resource not Found'});
-    }
-    
-    
-}));
+router.get('/:id', getProductById);
 
 export default router;
