7e7b18da6702a838b989977f6bbca8b0c7686da6
pavan-santhosh <125680930+pavan-santhosh@users.noreply.github.com>
2023-10-22 10:51:03 +0530

    LoginScreen

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -1,6 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit'
 
-const initalState = {
+const initialState = {
     userInfo: localStorage.getItem('userInfo')?JSON.parse(localStorage.getItem('userInfo')):null,
 }
 
