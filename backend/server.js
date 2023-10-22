6724c27f1cc149973f5fb3a298ea770c5faee5d0
pavan-santhosh <125680930+pavan-santhosh@users.noreply.github.com>
2023-10-22 17:46:41 +0530

    Order Routes & Controller

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
@@ -25,6 +26,7 @@ app.use(cookieParser());
 
 app.use("/api/products",productRoutes);
 app.use('/api/users',userRoutes);
+app.use('/api/order',orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
